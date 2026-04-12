73b8b2eb96cef2305eb75b94d97763a34abfe7f2
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-12 08:50:58 +0000

    add own homogenous example

---
+++ tests/test_new.rs
@@ -0,0 +1,37 @@
+#![feature(try_blocks)]
+#![feature(try_blocks_heterogeneous)]
+
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+struct Error1;
+
+#[derive(Debug)]
+struct Error2;
+
+impl From<ParseIntError> for Error1 {
+    fn from(_: ParseIntError) -> Self {
+        Self
+    }
+}
+
+impl From<ParseIntError> for Error2 {
+    fn from(_: ParseIntError) -> Self {
+        Self
+    }
+}
+
+fn err1(s: &str) -> Result<i32, Error1> {
+    Ok(s.parse()?)
+}
+
+fn err2(s: &str) -> Result<i32, Error2> {
+    Ok(s.parse()?)
+}
+
+#[test]
+fn homogenous() {
+    let x = try { err1("1")? + err1("2")? };
+    let y = try { err2("1")? + err2("2")? };
+    assert_eq!(x.unwrap(), y.unwrap());
+}
