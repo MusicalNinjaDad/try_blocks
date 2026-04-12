f697ebc9424c2752009aed664c3e0be8f0da324d
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-12 10:22:04 +0000

    show compiler error for missiing into

---
+++ tests/compilation/examples/fail_missing_into.rs
@@ -0,0 +1,44 @@
+#![feature(try_blocks)]
+#![feature(try_blocks_heterogeneous)]
+
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+struct Error1;
+
+#[derive(Debug)]
+struct Error2;
+
+impl From<ParseIntError> for Error1 {
+    fn from(_: ParseIntError) -> Self {
+        Self
+    }
+}
+
+impl From<ParseIntError> for Error2 {
+    fn from(_: ParseIntError) -> Self {
+        Self
+    }
+}
+
+impl From<Error2> for Error1 {
+    fn from(_: Error2) -> Self {
+        Self
+    }
+}
+
+fn err1(s: &str) -> Result<i32, Error1> {
+    Ok(s.parse()?)
+}
+
+fn err2(s: &str) -> Result<i32, Error2> {
+    Ok(s.parse()?)
+}
+
+fn heterogeneous_into_missing() {
+    let x = try bikeshed Result<_, Error2> { err1("1")? + err2("2")? };
+    let y = try bikeshed Result<_, Error2> { err2("1")? + err1("2")? };
+    assert_eq!(x.unwrap(), y.unwrap());
+}
+
+fn main() {}
