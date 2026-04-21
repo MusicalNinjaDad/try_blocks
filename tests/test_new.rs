726589484bbdeb3a9aa392a0d869d6c3937f3b89
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-21 09:04:56 +0000

    rationale & prior art

--- tests/test_new.rs
+++ tests/test_new.rs
@@ -93,6 +93,19 @@ fn heterogeneous_into_anyhow() {
 }
 
 #[test]
+fn homogeneous_via_into_turbofish() {
+    let x = try {
+        err1("1").map_err(Into::<anyhow::Error>::into)?
+            + err2("2").map_err(Into::<anyhow::Error>::into)?
+    };
+    let y = try {
+        err2("1").map_err(Into::<anyhow::Error>::into)?
+            + err1("2").map_err(Into::<anyhow::Error>::into)?
+    };
+    assert_eq!(x.unwrap(), y.unwrap());
+}
+
+#[test]
 fn heterogeneous_into_common_other() {
     let x = try bikeshed Result<_, Error1> { err2("1")? + err3("2")? };
     let y = try bikeshed Result<_, Error1> { err3("1")? + err2("2")? };
